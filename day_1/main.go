cf264f5b47d1e9672a97fcd9d486f88e3b575085
Nicolas Gasco <nicolas.gasco@qonto.com>
2025-12-04 10:59:05 +0100

    Refactor code to avoid repetition

--- day_1/main.go
+++ day_1/main.go
@@ -12,58 +12,12 @@ const minNum = 0
 const maxNum = 99 + 1 // +1 to compensate for 100 being equal to 0
 
 func main() {
-	partOne()
+	calcPassword(false)
 
-	partTwo()
+	calcPassword(true)
 }
 
-func partOne() {
-	data, err := os.ReadFile("input.txt")
-	if err != nil {
-		panic(err)
-	}
-
-	input := string(data)
-	rotations := strings.Split(input, "\n")
-
-	count := 0
-	currentNum := startingNum
-	for _, rotation := range rotations {
-		valueStr := strings.TrimSpace(rotation[1:])
-		value, err := strconv.Atoi(valueStr)
-		if err != nil {
-			panic(err)
-		}
-		value = value % maxNum // in case of large rotations
-
-		rawNewNum := currentNum
-
-		direction := rune(rotation[0])
-		switch direction {
-		case 'L':
-			rawNewNum -= value
-		case 'R':
-			rawNewNum += value
-		default:
-			panic("unknown direction")
-		}
-
-		rawNewNum = rawNewNum % maxNum
-
-		if rawNewNum < minNum {
-			currentNum = maxNum + rawNewNum
-		} else if rawNewNum > minNum {
-			currentNum = rawNewNum % maxNum
-		} else {
-			currentNum = rawNewNum
-			count++
-		}
-	}
-
-	fmt.Printf("The solution to part one of day 1 is: %d\n", count)
-}
-
-func partTwo() {
+func calcPassword(isSecurePassword bool) {
 	data, err := os.ReadFile("input.txt")
 	if err != nil {
 		panic(err)
@@ -81,7 +35,9 @@ func partTwo() {
 			panic(err)
 		}
 
-		count += int(value / maxNum)
+		if isSecurePassword {
+			count += int(value / maxNum)
+		}
 		value = value % maxNum // in case of large rotations
 
 		rawNewNum := currentNum
@@ -96,12 +52,13 @@ func partTwo() {
 			panic("unknown direction")
 		}
 
-		wentThroughZero := rawNewNum < minNum || rawNewNum > maxNum
-		isAtZero := currentNum == 0
-		if !isAtZero && wentThroughZero {
-			count++
+		if isSecurePassword {
+			wentThroughZero := rawNewNum < minNum || rawNewNum > maxNum
+			isAtZero := currentNum == 0
+			if !isAtZero && wentThroughZero {
+				count++
+			}
 		}
-
 		rawNewNum = rawNewNum % maxNum
 
 		if rawNewNum < minNum {
@@ -114,5 +71,9 @@ func partTwo() {
 		}
 	}
 
-	fmt.Printf("The solution to part two of day 1 is: %d\n", count)
+	if isSecurePassword {
+		fmt.Printf("The solution with complex password is: %d\n", count)
+	} else {
+		fmt.Printf("The solution with simple password is: %d\n", count)
+	}
 }
