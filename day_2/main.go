fb27d5327050aedc759c3aa13b3d87abd413d969
Nicolas Gasco <nicolas.gasco92@gmail.com>
2025-12-04 20:23:52 +0100

    Submit solution to Day 2

---
+++ day_2/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	startTime := time.Now()
+
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	input := string(data)
+	idRanges := strings.Split(input, ",")
+
+	sumPartOne := calculateInvalidIds(
+		calculateInvalidIdsParams{
+			idRanges: idRanges, isComplexId: false,
+		})
+	fmt.Printf("Solution to Day 2 - Part 1 is %d\n", sumPartOne)
+
+	sumPartTwo := calculateInvalidIds(
+		calculateInvalidIdsParams{
+			idRanges: idRanges, isComplexId: true,
+		})
+	fmt.Printf("Solution to Day 2 - Part 2 is %d\n", sumPartTwo)
+
+	fmt.Printf("Execution time: %s\n", time.Since(startTime))
+}
+
+type calculateInvalidIdsParams struct {
+	idRanges    []string
+	isComplexId bool
+}
+
+func calculateInvalidIds(params calculateInvalidIdsParams) int {
+	idRanges := params.idRanges
+	isComplexId := params.isComplexId
+
+	sum := 0
+
+	for _, idRange := range idRanges {
+		idRangeChunks := strings.Split(idRange, "-")
+		startId := idRangeChunks[0]
+		endId := idRangeChunks[1]
+
+		startIdInt, err := strconv.Atoi(startId)
+		if err != nil {
+			panic(err)
+		}
+
+		endIdInt, err := strconv.Atoi(endId)
+		if err != nil {
+			panic(err)
+		}
+
+		for i := startIdInt; i <= endIdInt; i++ {
+			isInvalid := false
+
+			idString := fmt.Sprintf("%d", i)
+			if isComplexId {
+				isInvalid = isInvalidIdPartTwo(idString)
+			} else {
+				isInvalid = isInvalidIdPartOne(idString)
+			}
+
+			if isInvalid {
+				sum += i
+			}
+		}
+	}
+
+	return sum
+}
+
+func isInvalidIdPartOne(id string) bool {
+	idLength := len(id)
+
+	patternLength := idLength / 2
+
+	pattern := id[:patternLength]
+	comparison := id[patternLength:]
+
+	return pattern == comparison
+}
+
+func isInvalidIdPartTwo(id string) bool {
+	const minPatternLength = 1
+
+	idLength := len(id)
+
+	highestPatternLength := idLength / 2
+
+	for patternLength := highestPatternLength; patternLength >= minPatternLength; patternLength-- {
+		if idLength%patternLength != 0 {
+			continue
+		}
+
+		pattern := id[0:patternLength]
+
+		var isInvalidId bool = false
+		for i := patternLength; i <= len(id)-patternLength; i += patternLength {
+
+			subPattern := id[i : i+patternLength]
+			if subPattern == pattern {
+				isInvalidId = true
+			} else {
+				isInvalidId = false
+				break
+			}
+		}
+		if isInvalidId {
+			return true
+		}
+	}
+	return false
+}
