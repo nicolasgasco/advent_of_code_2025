f0fe109242ba05996b9c7f0b03f716230eb73d8b
Nicolas Gasco <nicolas.gasco92@gmail.com>
2025-12-06 10:06:20 +0100

    Submit Day 5 - Part Two

--- day_5/main.go
+++ day_5/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 func main() {
@@ -19,23 +21,83 @@ func main() {
 	ranges := strings.Split(databaseSections[0], "\n")
 	ids := strings.Split(databaseSections[1], "\n")
 
-	freshIngredientsCount := 0
+	freshIngredientsCount := calculateFreshIngredientsCount(&ranges, &ids)
+	fmt.Printf("Solution to Day 5 - Part 1 is %d\n", freshIngredientsCount)
 
-	for _, id := range ids {
-		idValue, _ := strconv.Atoi(id)
+	freshIds := calculateFreshIds(&ranges)
+	fmt.Printf("Solution to Day 5 - Part 2 is %d\n", freshIds)
+}
 
-		for _, r := range ranges {
-			rangeValues := strings.Split(r, "-")
+func calculateFreshIds(ranges *[]string) int {
+	rangesInts := make([][2]int, len(*ranges))
+	for i, r := range *ranges {
+		rangeValues := strings.Split(r, "-")
+		start, _ := strconv.Atoi(rangeValues[0])
+		end, _ := strconv.Atoi(rangeValues[1])
+		rangesInts[i] = [2]int{start, end}
+	}
+	sort.Slice(rangesInts, func(i, j int) bool {
+		return rangesInts[i][0] < rangesInts[j][0]
+	})
 
-			start, _ := strconv.Atoi(rangeValues[0])
-			end, _ := strconv.Atoi(rangeValues[1])
+	uniqueRangesInts := make([][2]int, 0)
 
-			if idValue >= start && idValue <= end {
-				freshIngredientsCount++
-				break
+	for _, r := range rangesInts {
+		overlapFound := false
+		for ui, ur := range uniqueRangesInts {
+			isStartInRange := r[0] >= ur[0] && r[0] <= ur[1]
+			if isStartInRange {
+				overlapFound = true
+
+				isEndBeyondRange := r[1] > ur[1]
+				if isEndBeyondRange {
+					uniqueRangesInts[ui][1] = r[1]
+				}
 			}
+		}
+		if !overlapFound {
+			uniqueRangesInts = append(uniqueRangesInts, r)
 		}
 	}
 
-	fmt.Printf("Solution to Day 5 - Part 1 is %d\n", freshIngredientsCount)
+	totalIdsCount := 0
+	for _, ur := range uniqueRangesInts {
+		totalIdsCount += ur[1] - ur[0] + 1
+	}
+
+	return totalIdsCount
+}
+
+func calculateFreshIngredientsCount(ranges *[]string, ids *[]string) int {
+	freshIngredientsCount := 0
+
+	wc := sync.WaitGroup{}
+	mu := sync.Mutex{}
+
+	for _, id := range *ids {
+		idValue, _ := strconv.Atoi(id)
+
+		wc.Add(1)
+		go func() {
+			defer wc.Done()
+			for _, r := range *ranges {
+				rangeValues := strings.Split(r, "-")
+
+				start, _ := strconv.Atoi(rangeValues[0])
+				end, _ := strconv.Atoi(rangeValues[1])
+
+				if idValue >= start && idValue <= end {
+					mu.Lock()
+					freshIngredientsCount++
+					mu.Unlock()
+
+					break
+				}
+			}
+		}()
+	}
+
+	wc.Wait()
+
+	return freshIngredientsCount
 }
